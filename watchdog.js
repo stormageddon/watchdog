09a9d7d1c6fd56d184279402221a462f52f4b458
Michael Caputo <therubikscubekid@gmail.com>
2015-06-04 12:00:57 -0400

    Implement electron to allow application to live in the menu bar

--- watchdog.js
+++ watchdog.js
@@ -16,10 +16,21 @@ var currStreamers = [];
 var mainWindow = null;
 var appIcon = null;
 
+var window = global;
+window.$ = window.jQuery = require('./node_modules/jquery/dist/jquery.min.js');
+console.log(window.jQuery);
+
+// Need flash to display Twitch for now
+app.commandLine.appendSwitch('/Applications/Google Chrome.app/Contents/Versions/43.0.2357.81/Google Chrome Framework.framework/Internet Plug-Ins/PepperFlash/PepperFlashPlayer.plugin');
+
+// Specify flash version, for example, v17.0.0.169
+app.commandLine.appendSwitch('ppapi-flash-version', '17.0.0.188');
+
+
 var getFollowed = function(username, cb) {
   //curl -H 'Accept: application/vnd.twitchtv.v3+json' \
 // -X GET https://api.twitch.tv/kraken/users/test_user1/follows/channels
-
+  followed = [];
   request('https://api.twitch.tv/kraken/users/' + username + '/follows/channels', function(error, response, body) {
     if (!error) {
       var data = JSON.parse(body);
@@ -58,44 +69,70 @@ if (process.argv.length < 3) {
 
 var args = process.argv.slice(2);
 
+var contextMenu = {} // We don't want a new menu every time
 
-var dockMenu = Menu.buildFromTemplate([
-  { label: 'New Window', click: function() { console.log('New Window'); } },
-  { label: 'New Window with Settings', submenu: [
-    { label: 'Basic' },
-    { label: 'Pro'},
-  ]},
-  { label: 'New Command...'},
-]);
+var minutes = .5, the_interval = minutes * 60 * 1000;
+setInterval(function() {
+ tick();
+}, the_interval);
 
-app.dock.setMenu(dockMenu);
+var tick = function() {
+ console.log("Checking for new streamers");
+  getFollowed(args[0], function() {
+    console.log('========= Channels Online =========');
+    currStreamers = [];
+    async.each(followed, getChannelStatus, function(err) {
+      if(!err) {
 
-getFollowed(args[0], function() {
-  console.log('========= Channels Online =========');
+        var selectedStreamer = null;
+        var labels = [];
+        for (var i = 0; i < currStreamers.length; i++) {
+          labels.push({ label: currStreamers[i], type: 'normal', click: function(streamerName) { openStream(streamerName.label)}.bind(currStreamers[i]) });
+        }
 
-  async.each(followed, getChannelStatus, function(err) {
-    if(!err) {
-      appIcon = new Tray('/Users/Mike/Downloads/dota2.jpg');
-      var labels = [];
-      for (var i = 0; i < currStreamers.length; i++) {
-        labels.push({ label: currStreamers[i], type: 'radio' });
+        appIcon.setToolTip('Online streamers.');
+
+        console.log('currstreamers length:',currStreamers.length);
+        if (currStreamers.length === 0) {
+          console.log('set grey');
+          appIcon.setImage('/Users/Mike/Downloads/dota2_gray.jpg');
+          labels.push({label: 'No live streams', type: 'normal'});
+        }
+        else {
+          console.log('set not grey');
+          appIcon.setImage('/Users/Mike/Downloads/dota2.png');
+        }
+        labels.push({ label: 'Commands', type: 'separator' });
+        labels.push({ label: 'Quit', type: 'normal', click: close });
+        contextMenu = Menu.buildFromTemplate(labels);
+        appIcon.setContextMenu(contextMenu);
       }
-      var contextMenu = Menu.buildFromTemplate(labels);
-      appIcon.setToolTip('Online streamers.');
-      appIcon.setContextMenu(contextMenu);
-    }
-    else {
-      console.log('err:',err);
-    }
+      else {
+        console.log('err:',err);
+      }
+    });
   });
-});
+}
+
+var streamWindow = {};
+
+var openStream = function(streamerName) {
+  console.log("open stream", streamerName);
+  streamWindow = new BrowserWindow({width: 800, height: 600, "node-integration": false, 'web-preferences': {'plugins': true}} );
+  streamWindow.loadUrl('http://www.twitch.tv/' + streamerName);
+  streamWindow.on('closed', function() {
+    streamWindow = null;
+  });
+}
 
 app.on('ready', function() {
-/*  mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadUrl('http://google.com');
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });*/
-
-
+  appIcon = new Tray('/Users/Mike/Downloads/dota2_gray.jpg'); // Only need one Tray icon
+  tick();
 });
+
+var close = function() {
+  console.log('close app');
+  streamWindow = null;
+  appIcon.destroy();
+  process.exit(0);
+}
